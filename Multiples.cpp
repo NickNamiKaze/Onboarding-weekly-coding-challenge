342af48da5e445ceb14ac1bd778f3863b0c2b979
fnicolas2019 <francesconicolas99@gmail.com>
2023-05-02 15:30:43 -0400

    Create Multiples.cpp

---
+++ Multiples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int calculate(int x, int y){
+    //int total[] = [5];
+    //return total;
+}
+
+
+int main(){
+    cout << "Hello World\n";
+    cout << calculate(9,7);
+}
